a8c3c3cca0a60bae72519159d7c0bc395781bf6f
dustinhealy <dustinhealy1@gmail.com>
2022-06-01 19:03:12 -0700

    partially add IfC

--- src/Main.elm
+++ src/Main.elm
@@ -2,48 +2,95 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+
 type ExprC
     = NumC Float
     | BoolC Bool
     | StringC String
+    | IfC ExprC ExprC ExprC
+
+
+
+-- | MaybeIfC ExprC ExprC ExprC
+-- type MaybeIfC
+--   = IfC { cond : ExprC, ifT : ExprC, ifF : ExprC }
+--   | NotABool
+-- LamC
+-- AppC
+-- IfC
+
 
 type Value
     = NumV Float
     | BoolV Bool
     | StringV String
 
+
+
+-- CloV
+
+
 main =
-    StringC "Hi!"
+    IfC (BoolC True) (NumC 1) (NumC 0)
         |> interp
         |> serialize
         |> Html.text
 
+
+
+-- StringC "Hi!"
+--     |> interp
+--     |> serialize
+--     |> Html.text
 -- Interprets an Expression in JILI5.
+
+
 interp : ExprC -> Value
 interp expr =
     case expr of
         NumC n ->
             NumV n
+
         BoolC b ->
             BoolV b
+
         StringC s ->
             StringV s
 
+        IfC condition ifTrue ifFalse ->
+            case interp condition of
+                BoolV b ->
+                    if b then
+                        interp ifTrue
+
+                    else
+                        interp ifFalse
+
+                _ ->
+                    StringV "Error Placeholder (Cond not a bool)"
+
+
+
 -- Converts a JILI5 Value to its String representation.
+
+
 serialize : Value -> String
 serialize val =
     case val of
-        NumV f -> 
+        NumV f ->
             String.fromFloat f
+
         BoolV b ->
             stringFromBool b
+
         StringV s ->
             s
+
 
 stringFromBool : Bool -> String
 stringFromBool value =
     if value then
         "True"
+
     else
         "False"
-
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,48 +2,58 @@ module Tests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-
 import Main exposing (..)
+import Test exposing (..)
 
 
 interpTests : Test
 interpTests =
     describe "Interp"
         [ test "interp NumC" <|
-            \_ -> 
+            \_ ->
                 NumC 4
                     |> interp
                     |> Expect.equal (NumV 4)
         , test "interp StringC" <|
-            \_ -> 
+            \_ ->
                 StringC "Hey!"
                     |> interp
                     |> Expect.equal (StringV "Hey!")
         , test "interp BoolC" <|
-            \_ -> 
+            \_ ->
                 BoolC True
                     |> interp
                     |> Expect.equal (BoolV True)
+        , test "interp IfC" <|
+            \_ ->
+                IfC (BoolC True) (NumC 1) (NumC 2)
+                    |> interp
+                    |> Expect.equal (NumV 1)
         ]
+
 
 serializeTests : Test
 serializeTests =
     describe "Serialize"
         [ test "serialize NumV" <|
-            \_ -> 
+            \_ ->
                 NumV 4
                     |> serialize
                     |> Expect.equal "4"
         , test "serialize StringV" <|
-            \_ -> 
+            \_ ->
                 StringV "Hey!"
                     |> serialize
                     |> Expect.equal "Hey!"
         , test "serialize BoolV" <|
-            \_ -> 
+            \_ ->
                 BoolV True
                     |> serialize
                     |> Expect.equal "True"
+        , test "serialize IfC" <|
+            \_ ->
+                IfC (BoolC True) (NumC 1) (NumC 2)
+                    |> interp
+                    |> serialize
+                    |> Expect.equal "1"
         ]
-
